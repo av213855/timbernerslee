398a39b770af7074be00df3f773bd7e41e7fdd43
av213855 <105159825+av213855@users.noreply.github.com>
2022-11-22 19:00:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// fullpage customization
+$('#fullpage').fullpage({
+  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionSelector: '.vertical-scrolling',
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  normalScrollElements: '#scrolldiv'
+});
